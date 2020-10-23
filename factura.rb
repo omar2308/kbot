4b19fef1bbba08bc8623cba85025504a1fad2aad
Omar <omarfl7@gmail.com>
2020-10-23 04:44:14 +0000

    US-04 - Validacion cant. entera

--- factura.rb
+++ factura.rb
@@ -1,4 +1,7 @@
-cantidad= ARGV[0].to_i
-#puts "cantidad: #{cantidad}"
-total= cantidad*10*(1 - 0.0825)
-puts "#{total}"
+if(Integer(ARGV[0]) rescue false)
+    cantidad= ARGV[0].to_i
+    total= cantidad*10*(1 - 0.0825)
+    puts "#{total}"
+else
+    puts "Error: cantidad debe ser entero"
+end
