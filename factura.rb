eeb3d2f1c5e09c1fcc110270b87ac848540bb150
Omar <omarfl7@gmail.com>
2020-10-23 05:03:16 +0000

    US-05 - Dscto +1000, y refactorizaciones

--- factura.rb
+++ factura.rb
@@ -2,13 +2,22 @@ def is_number?(obj)
     true if Integer(obj) rescue false
 end
 
-impuesto = 0.0825
+impuesto_porc = 0.0825
 precio = 10
+dscto_1= 0.03
 cantidad = ARGV[0].to_i
-calcular_total = is_number? ARGV[0]
 
-if(calcular_total)
-    total= cantidad*precio*(1 + impuesto)
+subtotal = cantidad*precio
+
+impuesto = subtotal*impuesto_porc
+dscto = (subtotal > 1000) ? subtotal*dscto_1 : 0
+puts "subtotal: #{subtotal}, impuesto: #{impuesto}, dscto: #{dscto}"
+
+total = subtotal + impuesto - dscto
+
+mostrar_total = is_number? ARGV[0]
+
+if(mostrar_total)
     puts "#{total}"
 else
     puts "Error: cantidad debe ser entero"
