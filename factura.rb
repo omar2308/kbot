e8e1a6e1935ea50c8611a24006accbbf4dd8dd20
Omar <omarfl7@gmail.com>
2020-10-23 05:36:47 +0000

    US-06  - Dscto +1000, +5000

--- factura.rb
+++ factura.rb
@@ -2,23 +2,37 @@ class Factura
     def initialize(cantidad)
         @impuesto_porc = 0.0825
         @precio = 10
-        @dscto_1= 0.03
+        @dscto= 0
+        @subtotal = 0
+
         @cantidad = cantidad.to_i
-        @calcular = is_number?(@cantidad)
+        @calcular = is_number?(cantidad)
     end
 
     def is_number?(obj)
         return true if Integer(obj) rescue false
     end
 
+    def calcular_dscto()
+        case @subtotal
+        when 0..1000
+            @dscto = 0
+        when 1000..5000
+            @dscto = @subtotal*0.03
+        when 5000..
+            @dscto = @subtotal*0.05
+        end
+        puts "...dscto: #{@dscto}"
+    end
+
     def resultado()
         if (@calcular)
-            subtotal = @cantidad*@precio
-            impuesto = subtotal*@impuesto_porc
-            dscto = (subtotal > 1000) ? subtotal*@dscto_1 : 0
+            @subtotal = @cantidad*@precio
+            impuesto = @subtotal*@impuesto_porc
+            calcular_dscto()
 
-            #puts "subtotal: #{subtotal}, impuesto: #{impuesto}, dscto: #{dscto}"
-            total = subtotal + impuesto - dscto
+            puts "subtotal: #{@subtotal}, impuesto: #{impuesto}, dscto: #{@dscto}"
+            total = @subtotal + impuesto - @dscto
             puts "#{total}"
         else
             puts "Error: cantidad debe ser entero"
