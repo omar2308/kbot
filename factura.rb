8b76c055025f38335710eb5ce1539cff3acc2f79
Omar <omarfl7@gmail.com>
2020-10-23 05:15:51 +0000

    Se refactoriza pasando a clase

--- factura.rb
+++ factura.rb
@@ -1,24 +1,31 @@
-def is_number?(obj)
-    true if Integer(obj) rescue false
+class Factura
+    def initialize(cantidad)
+        @impuesto_porc = 0.0825
+        @precio = 10
+        @dscto_1= 0.03
+        @cantidad = cantidad.to_i
+        @calcular = is_number?(@cantidad)
+    end
+
+    def is_number?(obj)
+        return true if Integer(obj) rescue false
+    end
+
+    def resultado()
+        if (@calcular)
+            subtotal = @cantidad*@precio
+            impuesto = subtotal*@impuesto_porc
+            dscto = (subtotal > 1000) ? subtotal*@dscto_1 : 0
+
+            #puts "subtotal: #{subtotal}, impuesto: #{impuesto}, dscto: #{dscto}"
+            total = subtotal + impuesto - dscto
+            puts "#{total}"
+        else
+            puts "Error: cantidad debe ser entero"
+        end
+    end
 end
 
-impuesto_porc = 0.0825
-precio = 10
-dscto_1= 0.03
-cantidad = ARGV[0].to_i
 
-subtotal = cantidad*precio
-
-impuesto = subtotal*impuesto_porc
-dscto = (subtotal > 1000) ? subtotal*dscto_1 : 0
-puts "subtotal: #{subtotal}, impuesto: #{impuesto}, dscto: #{dscto}"
-
-total = subtotal + impuesto - dscto
-
-mostrar_total = is_number? ARGV[0]
-
-if(mostrar_total)
-    puts "#{total}"
-else
-    puts "Error: cantidad debe ser entero"
-end
+factura= Factura.new(ARGV[0])
+factura.resultado
