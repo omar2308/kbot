36d3cd0dca597fd49e90fab0b05b33053245fe1f
Omar <omarfl7@gmail.com>
2020-10-23 04:51:40 +0000

    Se corrige calculo de impuesto

--- factura.rb
+++ factura.rb
@@ -1,6 +1,14 @@
-if(Integer(ARGV[0]) rescue false)
-    cantidad= ARGV[0].to_i
-    total= cantidad*10*(1 - 0.0825)
+def is_number?(obj)
+    true if Integer(obj) rescue false
+end
+
+impuesto = 0.0825
+precio = 10
+cantidad = ARGV[0].to_i
+calcular_total = is_number? ARGV[0]
+
+if(calcular_total)
+    total= cantidad*precio*(1 + impuesto)
     puts "#{total}"
 else
     puts "Error: cantidad debe ser entero"
